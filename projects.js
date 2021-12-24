5bf1bbcc3f060c2f60d6c03b99ece0825e932f83
Sonu Kumar Kushwaha <flyingsonu736@gmail.com>
2021-12-25 02:33:59 +0530

    Update projects.js

--- projects.js
+++ projects.js
@@ -1,5 +1,45 @@
+const getAbout = async () => {
+  const aboutRes = await fetch("https://api.github.com/users/flyingsonu122");
+  const aboutData = await aboutRes.json();
+  //   console.log(aboutData);
+  const d = new Date(aboutData.created_at).toDateString();
+  var company = aboutData.company.replace("@", "");
+  // console.log(company);
+  var email;
+  if(aboutData.email == null) {
+    email = '';
+  } else {
+    email = `mailto: ${aboutData.email}`;
+  }
+
+  const about = document.getElementById("about");
+
+  let aboutTemplate = `
+      <a href = "${aboutData.html_url}" target = "_blank" rel = "noopener noreferrer" data-aos="fade-up" > 
+      <img src="${aboutData.avatar_url}" alt="${aboutData.name}" class="pic">
+      </a>
+      <h1>${aboutData.name}</h1>
+      <h5>${aboutData.bio}</h5>
+      <h3>${aboutData.location}</h3>
+      <a href = "https://github.com/${aboutData.login}" target = "_blank" rel = "noopener noreferrer" data-aos="fade-up" class="fa fa-github"> 
+      @${aboutData.login}
+      </a>
+      <p>Joined on: ${d}</p>
+      <p><a href="${email}" target="_blank" rel="noopener noreferrer">${email}</a></p>
+      <a href = "https://twitter.com/${aboutData.twitter_username}" target = "_blank" rel = "noopener noreferrer" data-aos="fade-up" class="fa fa-twitter"> 
+       @${aboutData.twitter_username}
+      </a>
+      <p><a href="${aboutData.blog}" target="_blank" rel="noopener noreferrer">${aboutData.blog}</a></p>
+      <h5>followers: ${aboutData.followers} following: ${aboutData.following}</h5>
+      <p>company :<a href="https://github.com/${company}" target="_blank" rel="noopener noreferrer">${aboutData.company}</a></p>
+  `;
+  about.innerHTML += aboutTemplate;
+}
+
+
 const getData = async () => {
-  let uri = `https://api.github.com/users/flyingsonu122/repos?per_page=100`;
+  getAbout();
+  let uri = "https://api.github.com/users/flyingsonu122/repos?per_page=100";
   const res = await fetch(uri);
   const data = await res.json();
 
@@ -23,13 +63,13 @@ const getData = async () => {
     const projects = document.getElementById("projects");
 
     let template = `
-                <a href="${i.html_url}" target="_blank" rel="noopener noreferrer" data-aos="fade-up">
-                    <img src="https://github-readme-stats.vercel.app/api/pin/?username=flyingsonu122&repo=${i.name}&theme=buefy" alt="${i.name}">
-                </a>
-        `;
+      <a href="${i.html_url}" target="_blank" rel="noopener noreferrer" data-aos="fade-up" >
+          <img src="https://github-readme-stats.vercel.app/api/pin/?username=flyingsonu122&repo=${i.name}&theme=buefy" alt="${i.name}">
+      </a>
+    `;
 
-    // projects.innerHTML += template;
-    projects.insertAdjacentHTML('beforeend', template);
+    projects.innerHTML += template;
+    // projects.insertAdjacentHTML('beforeend', template);
   });
 };
 
