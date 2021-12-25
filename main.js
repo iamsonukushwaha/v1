166c2f2bb1d3eea9eefa60d65bdcf5e137b1da11
Sonu Kumar Kushwaha <flyingsonu736@gmail.com>
2021-12-25 19:04:34 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,42 +1,38 @@
-const getProjects = async() => {
-    let uri = `https://www.sonu.live/v1/projects.json`;
+
+const getData = async () => {
+    let uri = "https://api.github.com/users/flyingsonu122/repos?per_page=100";
     const res = await fetch(uri);
     const data = await res.json();
 
-    let list = data.projects;
-    // console.log(list);
+    // console.log(data);
 
-
-    list.forEach(i => {
-        const row = document.getElementById('row');
-
-
-        // console.log(row);
+    data.forEach((i) => {
         // console.log(i);
         // console.log(i.name);
+        // console.log(i.description);
+        // console.log(i.owner.login);
+        // console.log(i.html_url);
+        // console.log(i.license.name);
+        // console.log(i.open_issues);
+        // console.log(i.created_at);
+        // console.log(i.pushed_at);
+        // console.log(i.size);
+        // console.log(i.stargazers_count);
+        // console.log(i.watchers_count);
+        // console.log(i.default_branch);
+
+
+        const projects = document.getElementById('projects');
 
         let template = `
-            <div class="fullCard" data-aos="fade-up">
-                    <div class="cardContent">
-                        <div class="cardText">
-                            <h4>${i.name}</h4>
-                            <a href="${i.link}" target="_blank" rel="noopener noreferrer">
-                                <img src="${i.image}" alt="${i.name}">
-                            </a>
-                        </div>
-                        <div class="cardLinks">
-                            <a href="${i.link}" target="_blank" class="links">Live Demo</a>
-                            <a href="${i.source}" target="_blank" class="links">See Code</a>
-
-                        </div>
-                    </div>
-                </div>
+            <a href="${i.html_url}" target="_blank" rel="noopener noreferrer" data-aos="fade-up" >
+                <img src="https://github-readme-stats.vercel.app/api/pin/?username=flyingsonu122&repo=${i.name}&theme=buefy" alt="${i.name}">
+            </a>
         `;
 
-        row.innerHTML += template;
-       // row.insertAdjacentHTML('beforeend', template);
+        projects.innerHTML += template;
+        // projects.insertAdjacentHTML('beforeend', template);
     });
+};
 
-}
-
-window.addEventListener('DOMContentLoaded', () => getProjects());
+window.addEventListener("DOMContentLoaded", () => getData());
